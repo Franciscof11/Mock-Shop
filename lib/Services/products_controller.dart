ce34cde5763e0f218d9610141acf272e0a33cfd6
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-01-19 15:11:32 -0300

    feat : Products Controller config

--- lib/Services/products_controller.dart
+++ lib/Services/products_controller.dart
@@ -1,3 +1,24 @@
 import 'package:get/get.dart';
 
-class ProductsController extends GetxController {}
+import 'Products_repository.dart';
+
+class ProductsController extends GetxController {
+  ProductsRepository productsRepository = ProductsRepository();
+  RxBool loading = false.obs;
+  List products = [].obs;
+  var showGrid = false.obs;
+
+  ProductsController() {
+    loadProductsFromRepo();
+  }
+
+  loadProductsFromRepo() async {
+    loading(true);
+    products = await productsRepository.getAllProducts();
+    loading(false);
+  }
+
+  toggleGrid() {
+    showGrid(!showGrid.value);
+  }
+}
