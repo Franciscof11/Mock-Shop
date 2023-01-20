c4b00b3d55b24b64275ab2e8a11536c5c9d90e7a
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-01-20 17:30:01 -0300

    refactor : Home App Bar

--- lib/Home_page/Widgets/app_bar.dart
+++ lib/Home_page/Widgets/app_bar.dart
@@ -6,49 +6,54 @@ class HomeAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        const SizedBox(width: 22),
-        SvgPicture.asset(
-          'assets/icons/app_bar_menu_icon.svg',
-        ),
-        const SizedBox(width: 50),
-        Container(
-          width: 270,
-          constraints: const BoxConstraints(
-            maxHeight: 50,
-          ),
-          child: TextFormField(
-            decoration: InputDecoration(
-              focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Color.fromRGBO(144, 149, 166, 0.5),
+    return SafeArea(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(width: 22),
+            SvgPicture.asset(
+              'assets/icons/app_bar_menu_icon.svg',
+            ),
+            const SizedBox(width: 50),
+            Container(
+              width: 270,
+              constraints: const BoxConstraints(
+                maxHeight: 50,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Color.fromRGBO(144, 149, 166, 0.5),
+                      ),
+                      borderRadius: BorderRadius.circular(40)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40)),
+                  hintText: 'Search Product',
+                  hintStyle: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Color.fromRGBO(27, 21, 61, 0.55),
                   ),
-                  borderRadius: BorderRadius.circular(40)),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(40)),
-              hintText: 'Search Product',
-              hintStyle: const TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                color: Color.fromRGBO(27, 21, 61, 0.55),
-              ),
-              labelStyle: const TextStyle(
-                color: Colors.grey,
-                fontWeight: FontWeight.w400,
-              ),
-              prefixIcon: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 18),
-                child: SvgPicture.asset(
-                  'assets/icons/app_bar_search_icon.svg',
+                  labelStyle: const TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 18),
+                    child: SvgPicture.asset(
+                      'assets/icons/app_bar_search_icon.svg',
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-        )
-      ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
